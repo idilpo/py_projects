b9afd0bf931554c37d221a4bce138808d6522045
idilpo <>
2021-05-26 09:57:57 +0300

    latest changes

--- Global AI Hub/day2_hw1.py
+++ Global AI Hub/day2_hw1.py
@@ -2,12 +2,12 @@
 # Merge these two lists. Multiply all values in the new list by 2.
 # Use a loop to print the data type of the all values in the new list.
 
-# Comment on the exercise: here the instruction "number" are considered as "digits" to avoid infinite loops.
+# Comment on the exercise: for the sake of an implementable answer, here the instruction "number" is considered as "digits".
 odd_digits = [1, 3, 5, 7, 9]
 even_digits = [0, 2, 4, 6, 8]
 merged_list = odd_digits + even_digits #two lists are merged together using the built-in function "merge"
-merged_list.sort() #the merged list is sorted in an incrasing order with the buil-in function "sort"
-for i in merged_list: #looping thorugh the elements in the list, multiplying each by 2
+merged_list.sort() #the merged list is sorted in an increasing order with the buil-in function "sort"
+for i in merged_list: #looping through the elements in the list, multiplying each by 2
     merged_list[i] *= 2
     print("Element in index[%d]: %d" % (i, merged_list[i]))
 print("Updated list: {}" .format(merged_list))
